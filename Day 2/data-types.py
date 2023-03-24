09471f6879a7b28b89ca4980dbfd032d43bf7ab8
Ignacio Royo-Villanova <Ignacio.RoyoVillanova@cognizant.com>
2023-03-24 10:55:11 +0100

    Day 2

---
+++ Day 2/data-types.py
@@ -0,0 +1,26 @@
+# Data types
+# String Integer float Boolean 
+
+# String:
+
+print("Hello"[4])
+print("Hello" + "World")
+
+#Integer
+
+print(123)
+print(123 + 1)
+print(123_256)
+print(123.256)
+
+#Boolean
+
+print(True)
+print(False)
+
+# type conversion
+
+string = str(5)
+print(type(string))
+print(int(string))
+print(float(string))
