d14b1145adb8afbe32bf0f11809fb46d7c08b0d0
Ignacio Royo-Villanova <Ignacio.RoyoVillanova@cognizant.com>
2023-03-28 13:01:30 +0200

    added first six days of 30 Days Of Python

---
+++ Day 4/ramdom.py
@@ -0,0 +1,11 @@
+import random
+
+import my_module
+print(my_module.pi)
+
+random_int = random.randint(0, 1)
+print(random_int)
+
+random_float = random.random() * 5
+print(random_float)
+print(f"Your random number si {random_float}")
